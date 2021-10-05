688200b244b36d28841151bbf7ac3bbf6b40c3b9
ThierryWouters <thierry@thierrywouters.com>
2021-10-05 14:29:22 +0200

    original

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
+
+console.log("Test: Website is loading")
 
 function App() {
+
+  const API_URL = ' https://pokeapi.co/api/v2/'
+  const API_CALL = (API_URL + 'pokemon/' + pokemonID)
   return (
     <div>
-      Begin hier met de opdracht!
+
+
     </div>
   );
 }
